422e289eb1744d889931ed4438370b605aa7271e
AlfredoJSpera <153198209+AlfredoJSpera@users.noreply.github.com>
2023-12-19 17:27:44 +0100

    Update ObjectSerializer.java
    Made the methods generic.
    Made the deserialize method return an already casted object

--- src/ObjectSerializer.java
+++ src/ObjectSerializer.java
@@ -1,18 +1,32 @@
 import java.io.*;
 
-public class ObjectSerializer {
-	public static void serialize(Object obj, String path) throws IOException {
-		FileOutputStream fos = new FileOutputStream(path);
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(obj);
+public class ObjectSerializer<T> {
+
+	/**
+	 * Serializes an object to a file.
+	 * @param path The path to the file.
+	 * @param object The object to be serialized. Must implement Serializable.
+	 * @throws IOException
+	 */
+	public static <T extends Serializable> void serialize(String path, T object) throws IOException {
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
+		oos.writeObject(object);
 		oos.close();
 	}
 
-	public static Object deserialize(String path) throws IOException, ClassNotFoundException {
-		FileInputStream fis = new FileInputStream(path);
-		ObjectInputStream ois = new ObjectInputStream(fis);
-		Object object = ois.readObject();
+	/**
+	 * Deserializes an object from a file.
+	 * @param path The path to the file.
+	 * @param targetClass The class of the object to be deserialized.
+	 * @return The deserialized object.
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	public static <T> T deserialize(String path, Class<T> targetClass) throws IOException, ClassNotFoundException {
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
+		T object = targetClass.cast(ois.readObject());
 		ois.close();
 		return object;
 	}
+
 }
